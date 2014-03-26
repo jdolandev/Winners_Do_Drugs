7fe73c6a86ac10c236bc51cf25822d0e34405c9a
jdolandev <jdolandev@gmail.com>
2014-03-26 03:43:18 -0400

    updated batch comments
    gotta' let 'em all know wot' i' 's 'out

--- winnersusedrugs.bat
+++ winnersusedrugs.bat
@@ -1,3 +1,7 @@
+:: This is the batch file to run it on windows. I developed this puppy on windows,
+:: So that's why there's a .bat
+:: Don't worry, kids, this game'll run on linux and OSX too.
+
 @echo off
 
 start "" "%PROGRAMFILES%\LOVE\love" .
