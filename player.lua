5453ceb5b33f68f23e86d50adf7030f7d4b16449
jdolandev <jdolandev@gmail.com>
2014-03-26 03:41:18 -0400

    Whitespace removal
    Makes it look like I know more than I do

--- player.lua
+++ player.lua
@@ -15,67 +15,39 @@ function player:update(dt)
 
 	--Move left
 	if love.keyboard.isDown("a") then
-
 		if (self.x > 0) then
-		
 			if self.hspeed > -self.maxH then
-
 				self.hspeed = self.hspeed - 0.2
-			
 			end
-		
 			self.x = math.floor(self.x + self.hspeed)
-		
 		end
-
 	--Move right
 	elseif love.keyboard.isDown("d") then
-
 		if self.x + self.width < love.graphics.getWidth() then
-
 			if self.hspeed < self.maxH then
-
 				self.hspeed = self.hspeed + 0.2
-
 			end 
-
 			if self.hspeed < 0 then
-
 				self.hspeed = self.hspeed + 0.4
-
 			end
-
 			self.x = math.floor(self.x + self.hspeed)
-
 		end
-
 	--If no keys are being pressed
 	else
 		if self.hspeed > 0 then
-
 			self.hspeed = self.hspeed - 0.4
-
 		elseif self.hspeed < 0 then
-			
 			self.hspeed = self.hspeed + 0.4
-		
 		end
 	end
-
 	--Check wall collision
 	if self.x <= 0 then
-
 		self.x = 0
-
 	elseif self.x >= love.graphics.getWidth() - self.width then
-		
 		self.x = love.graphics.getWidth() - self.width
-	
 	end
-
 	--Gravity
 	if self.y < ( love.graphics.getHeight() - 32 ) then
-
 		self.y = self.y + self.vspeed
 		self.vspeed = self.vspeed + 0.3
 		self.x = self.x - ((self.vspeed / self.width) * 2 )
@@ -86,14 +58,11 @@ function player:update(dt)
 		self.width = 32
 		self.y = love.graphics.getHeight() - 32
 		self.vspeed = 0;
-	
-	end
 
+	end
 	--Jumping
 	if love.keyboard.isDown(" ") then
-
 		if self.y == love.graphics.getHeight() - 32 then
-		
 			self.y = self.y - 1
 			self.vspeed = -7.0
 		end
@@ -101,7 +70,5 @@ function player:update(dt)
 end
 
 function player:draw()
-
 	love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
-
 end
